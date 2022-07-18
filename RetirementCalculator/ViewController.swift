a3f6647677ad5a8064292909afb93038532af001
sonisuman <sonisuman67@gmail.com>
2022-07-18 08:04:04 -0400

    Implement feature 3.

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -32,6 +32,20 @@ class ViewController: UIViewController {
     Analytics.trackEvent("Nevigated_to_Calculator.")
   }
 
+  func calculateRetirementAmount(current_age: Int, retirement_age : Int, monthly_investment: Float, current_savings: Float, interest_rate: Float) -> Double {
+      let months_until_retirement = (retirement_age - current_age) * 12
+      
+      var retirement_amount = Double(current_savings) * pow(Double(1+interest_rate/100), Double(months_until_retirement))
+      
+      for i in 1...months_until_retirement {
+          let monthly_rate = interest_rate / 100 / 12
+          retirement_amount += Double(monthly_investment) * pow(Double(1+monthly_rate), Double(i))
+      }
+      
+      return retirement_amount
+  }
+  
+  
   @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
     //Crashes.generateTestCrash()
     let current_age : Int? = Int(ageTextField.text!)
@@ -40,8 +54,9 @@ class ViewController: UIViewController {
     let current_savings : Float? = Float(savingsTextField.text!)
     let interest_rate : Float? = Float(interestRateTextField.text!)
     
-    resultLabel.text = "If you save $\(monthly_investment!) every month for \(planned_retirement_age! - current_age!) years, and invest that money plus your current investment of $\(current_savings!) at a \(interest_rate!)% anual interest rate, you will have $X by the time you are \(planned_retirement_age!)"
+    let retirement_amount = calculateRetirementAmount(current_age: current_age!, retirement_age: planned_retirement_age!, monthly_investment: monthly_investment!, current_savings: current_savings!, interest_rate: interest_rate!)
     
+    resultLabel.text = "If you save $\(monthly_investment!) every month for \(planned_retirement_age! - current_age!) years, and invest that money plus your current investment of $\(current_savings!) at a \(interest_rate!)% anual interest rate, you will have $\(retirement_amount) by the time you are \(planned_retirement_age!)"
     let properties = ["current_age": String(current_age!),
                       "planned_retirement_age": String(planned_retirement_age!)]
     
